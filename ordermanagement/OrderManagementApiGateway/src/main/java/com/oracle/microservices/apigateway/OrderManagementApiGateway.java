eb890014bd057f7503967dc5ba01a58e21148229
sgunjur <sgunjur@SGUNJUR-IN.in.oracle.com>
2020-05-22 16:47:16 +0530

    updated application.xml,pom.xml

--- ordermanagement/OrderManagementApiGateway/src/main/java/com/oracle/microservices/apigateway/OrderManagementApiGateway.java
+++ ordermanagement/OrderManagementApiGateway/src/main/java/com/oracle/microservices/apigateway/OrderManagementApiGateway.java
@@ -1,11 +1,12 @@
 package com.oracle.microservices.apigateway;
 
 
+import com.oracle.microservices.apigateway.web.filter.ZuulPreFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -15,4 +16,5 @@ public class OrderManagementApiGateway {
     public static void main(String[] args) {
         SpringApplication.run(OrderManagementApiGateway.class, args);
     }
+
 }
