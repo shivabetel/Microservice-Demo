cdb4501cd288c06d6deeb4726b9ee4ac7178a441
shiva <gyan.soni@oracle.com>
2021-06-29 13:23:28 +0530

    insert scripts for products and product category

---
+++ ordermanagement/product_category.sql
@@ -0,0 +1,7 @@
+/*
+-- Query: SELECT * FROM ordermanagement.product_category
+LIMIT 0, 1000
+
+-- Date: 2021-06-29 13:22
+*/
+INSERT INTO `` (`product_category_cd`,`product_category_desc`,`product_category_name`) VALUES ('1','Electronics','ELECTRONICS');
---
+++ ordermanagement/products.sql
@@ -0,0 +1,11 @@
+/*
+-- Query: SELECT * FROM ordermanagement.product
+LIMIT 0, 1000
+
+-- Date: 2021-06-29 13:21
+*/
+INSERT INTO ordermanagement.product (`product_id`,`active`,`product_description`,`image`,`product_name`,`price`,`category`) VALUES (1,'1','LG 65inches 4k Utlra HD',NULL,'LG 65inches 4k Utlra HD',18000,'1');
+INSERT INTO ordermanagement.product (`product_id`,`active`,`product_description`,`image`,`product_name`,`price`,`category`) VALUES (2,'1','LG 139 cm (55 Inches) 4K UHD OLED Smart TV OLED55C8PTA (Black) (2018 model)',NULL,'LG 139 cm (55 Inches)',20000,'1');
+INSERT INTO ordermanagement.product (`product_id`,`active`,`product_description`,`image`,`product_name`,`price`,`category`) VALUES (3,'1','Mi TV 4A PRO 80 cm (32 inches) HD Ready Android LED TV (Black)\"',NULL,'Mi TV 4A PRO 80 cm (32 inches)',25000,'1');
+INSERT INTO ordermanagement.product (`product_id`,`active`,`product_description`,`image`,`product_name`,`price`,`category`) VALUES (5,'1','Sony Bravia 80 cm (32 inches) ',NULL,'Sony Bravia 80 cm (32 inches) ',42000,'1');
+INSERT INTO ordermanagement.product (`product_id`,`active`,`product_description`,`image`,`product_name`,`price`,`category`) VALUES (6,'1','Sony Bravia 80 cm (32 inches) HD Ready LED TV KLV-32R202G (Dark Brown)',NULL,'Sony Bravia 80 cm (32 inches) ',65000,'1');
